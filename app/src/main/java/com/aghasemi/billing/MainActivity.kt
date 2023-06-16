455c579adbd868e291453a6a13dcab13663e6712
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-06-16 11:58:49 +0330

    add navigation drawer

--- app/src/main/java/com/aghasemi/billing/MainActivity.kt
+++ app/src/main/java/com/aghasemi/billing/MainActivity.kt
@@ -1,35 +1,59 @@
 package com.aghasemi.billing
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.aghasemi.billing.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolbar)
 
-        val navView: BottomNavigationView = binding.navView
+        val drawer = binding.drawerLayout
+        val navDrawer: NavigationView = binding.navDrawer
+        val bottomNavigationView: BottomNavigationView = binding.appBarMain.contentMain.bottomNavView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.nav_gallery
         )
+            .setOpenableLayout(drawer)
+            .build()
+
+        /*var appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.nav_gallery
+            )
+        )*/
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
+        navDrawer.setupWithNavController(navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(this, R.id.nav_host_fragment_content_main)
+        return (navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp())
     }
 }
