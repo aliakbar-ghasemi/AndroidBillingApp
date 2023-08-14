1f6425cbf03d264699b0d62a41ead6bf8e7b4af8
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-08-14 18:43:55 +0330

    bug fix bottom navbar visible only in main pages

--- app/src/main/java/com/aghasemi/billing/ui/MainActivity.kt
+++ app/src/main/java/com/aghasemi/billing/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.aghasemi.billing.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
@@ -36,19 +37,24 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_home,
-            R.id.navigation_dashboard,
-            R.id.navigation_notifications,
-            R.id.nav_category
+        val mainPageDestinationList = listOf(
+            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
         )
-            .setOpenableLayout(drawer)
-            .build()
+
+        appBarConfiguration = AppBarConfiguration.Builder(
+            *mainPageDestinationList.toIntArray()
+        ).setOpenableLayout(drawer).build()
 
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
         navDrawer.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            val id = destination.id
+            val isMainPageDestination = mainPageDestinationList.indexOf(id) < 0
+            bottomNavigationView.visibility = if (isMainPageDestination) View.GONE else View.VISIBLE
+        }
 
         //update locale
         val configuration = resources.configuration
@@ -59,7 +65,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(this, R.id.nav_host_fragment_content_main)
-        return (navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp())
+        return (navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp())
     }
 }
